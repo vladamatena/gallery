239cc081686520ed520aa63d888367d360f0805a
Vladimír Matěna <vlada.matena@gmail.com>
2014-04-06 19:11:01 +0200

    Simple gallery object, listing root directory as json

--- gallery.js
+++ gallery.js
@@ -1,6 +1,34 @@
-gallery = {
-	version: 0,
-	init: function(wrapper) {
-		$(wrapper).html("Gallery initialized");
-	}
+function Gallery(wrapper, api) {
+	/// Gallery version
+	this.version = 0;
+	
+	/// Gallery wrapping element
+	var $wrapper = $(wrapper);
+	/// Gallery API URL
+	var api = api;
+	/// Path to current directory
+	var path = "."
+	
+	/** Gallery initialization */
+	init = function() {
+		// Render root directory
+		cd(".");
+	};
+	
+	/** Change directory */
+	cd = function(path) {
+		$.ajax({
+			url: api,
+			data: { fn: 'ls', folder: path }
+		}).done(function(items) {
+			renderDir(items);
+		});
+	};
+	
+	var renderDir = function(items) {
+		$wrapper.html(items);
+	};
+	
+	// Initialize gallery
+	init();
 }
