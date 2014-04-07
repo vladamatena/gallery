cc2be91849b0cb9bf94cd336fbaf2452e47ce36f
Vladimír Matěna <vlada.matena@gmail.com>
2014-04-07 18:08:45 +0200

    Draft viewer element layout

--- gallery.js
+++ gallery.js
@@ -9,29 +9,67 @@ function Gallery(wrapper, api) {
 	var $gallery = null;
 	var $path = null;
 	var $listing = null;
+	var $viewer = null;
 	
 	/// Gallery API URL
 	var api = api;
 	/// Path to current directory
 	var path = null;
+	/// Current directory content
+	var items = null;
+	/// Current open image index relative to items
+	var currentImage = null;
 	
-	var gallerytemplate = '<div class="gallery"><div class="path"></div><div class="listing"></div></div>'
-	var itemDirectoryTemplate = '{{#.}}<div class="item {{type}}" data-name="{{name}}"><span class="name">{{name}}</span></div>{{/.}}';
-	var itemImageTemplate = '{{#.}}<div class="item {{type}}" data-name="{{name}}"><div class="thumb" style="background-image: url(\'{{src}}\');"></div><span class="name">{{name}}</span></div>{{/.}}';
-	var itemCategorizedListTemplate = '<div class="category">{{category}}</div>{{#dirs}}<div class="item {{type}}" data-name="{{name}}"><span class="name">{{name}}</span></div>{{/dirs}}';
+	var gallerytemplate = '\
+		<div class="gallery">\
+			<div class="path"></div>\
+			<div class="listing"></div>\
+			<div class="viewer">\
+				<div class="prev">Prev</div>\
+				<div class="next">Next</div>\
+				<div class="menu">\
+					<div class="info">Informations</div>\
+					<div class="name">NAME</div>\
+					<div class="exit">Exit</div>\
+				</div>\
+			</div>\
+		</div>'
+	var itemDirectoryTemplate = '\
+		{{#.}}\
+			<div class="item {{type}}" data-name="{{name}}">\
+				<span class="name">{{name}}</span>\
+			</div>\
+		{{/.}}';
+	var itemImageTemplate = '\
+		{{#.}}\
+			<div class="item {{type}}" data-name="{{name}}">\
+				<div class="thumb" style="background-image: url(\'{{thumb}}\');"></div>\
+				<span class="name">{{name}}</span>\
+			</div>\
+		{{/.}}';
+	var itemCategorizedListTemplate = '\
+		<div class="category">{{category}}</div>\
+		{{#dirs}}\
+			<div class="item {{type}}" data-name="{{name}}">\
+				<span class="name">{{name}}</span>\
+			</div>\
+		{{/dirs}}';
 	var itemListingBreakTemplate = '<div class="listing-break"></div>';
-	var pathPartTemplate = '{{#.}}<span class="path-part" data-path="{{path}}">{{name}}</span>{{/.}}';
+	var pathPartTemplate = '\
+		{{#.}}\
+			<span class="path-part" data-path="{{path}}">{{name}}</span>\
+		{{/.}}';
 		
 	/** Gallery initialization */
 	init = function() {
-		// Render gallery element
+		// Render gallery and viewer element
 		$wrapper.empty();
-		$(gallerytemplate).appendTo($wrapper);
+		$gallery = $(gallerytemplate).appendTo($wrapper);
 		
 		// Resolve gallery element references
-		$gallery = $wrapper.find('.gallery');
 		$listing = $gallery.find('.listing');
 		$path = $gallery.find('.path');
+		$viewer = $gallery.find('.viewer');
 		
 		// Render root directory
 		cd("");
@@ -95,6 +133,7 @@ function Gallery(wrapper, api) {
 	}
 	
 	var renderDirCategorized = function(dirs) {
+		// Categorize items by year contained in name
 		dirs = sortItems(dirs);
 		var categ = {};
 		$(dirs).each(function() {
@@ -108,7 +147,6 @@ function Gallery(wrapper, api) {
 		});
 		
 		var ret = "";
-		
 		for(cat in categ)
 			ret += Mustache.render(itemCategorizedListTemplate, {category: cat, dirs: categ[cat]});
 		
@@ -121,9 +159,15 @@ function Gallery(wrapper, api) {
 	}
 	
 	var renderDir = function(items, categorize) {
+		// Set current directory content
+		items = sortItems(items);
+		self.items = items;
+		
 		// Add data to items
 		$(items).each(function() {
-			this.src = api + "?fn=thumb&img=" + self.path + "/" + this.name;
+			this.thumb = api + "?fn=thumb&img=" + self.path + "/" + this.name;
+			this.web = api + "?fn=web&img=" + self.path + "/" + this.name;
+			this.src = api + "?fn=img&img=" + self.path + "/" + this.name;
 		});
 		
 		// Render elements
@@ -156,7 +200,24 @@ function Gallery(wrapper, api) {
 			else
 				cd(self.path + "/" + name);
 		});
+		
+		// Register image click events
+		$('.item.image', $listing).click(function() {
+			var name = $(this).data('name');
+			// Find index for name
+			var index = 0;
+			$(self.items).each(function(i) {
+				if(this.name == name)
+					index = i;
+			});
+			displayImage(index);
+		});
 	};
+	
+	var displayImage = function(index) {
+		alert(index);
+		
+	}
 	
 	// Initialize gallery
 	init();
